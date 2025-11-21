23589cbc0221701aac2fd9f45c17d99eb7233b10
ShaikInthiyaz-3246 <shaikinthiyaz3246@gmail.com>
2025-11-22 00:03:08 +0530

    Create  PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,15 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if (str.equals(rev))
+            System.out.println(str + " is a Palindrome");
+        else
+            System.out.println(str + " is NOT a Palindrome");
+    }
+}

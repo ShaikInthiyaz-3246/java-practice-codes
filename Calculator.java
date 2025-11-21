7ee29cbdb4ac6a372cbf8ebf215ecb0a75d4bab1
ShaikInthiyaz-3246 <shaikinthiyaz3246@gmail.com>
2025-11-21 23:52:18 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        int difference = b - a;
+        System.out.println("Difference: " + difference);
+    }
+}
